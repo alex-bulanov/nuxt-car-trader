495834e1613a99b74c3540c1e4e50acb2a236243
alex-bulanov.mail <alex-bulanov.mail@yandex.ru>
2024-06-20 12:25:43 +0300

    Главная. Header для сайта

--- pages/index.vue
+++ pages/index.vue
@@ -5,5 +5,15 @@ useHead({
 </script>
 
 <template>
-	<div>Welcome</div>
+	<div>
+		<!-- HEADER -->
+		<header class="header sticky top-0 left-0 z-50 border-b py-4 bg-white shadow">
+			<UContainer>
+				<div class="header__wrapper flex justify-between items-center gap-1">
+					<a class="font-mono text-3xl" href="/">Cartrader</a>
+				</div>
+			</UContainer>
+		</header>
+		<!-- HEADER -->
+	</div>
 </template>

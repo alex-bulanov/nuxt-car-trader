5630d6ad17a11e3462c2f1ba5f785cc5048109fe
alex-bulanov.mail <alex-bulanov.mail@yandex.ru>
2024-06-21 12:35:48 +0300

    Реализация вложенных страниц

---
+++ pages/city/[city]/car.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts"></script>
+
+<template>
+	<div class="cars-page">
+		<main class="cars-page__main py-25">
+			<UContainer>
+				<div class="cars-page__wrapper grid grid-cols-4 gap-x-4 gap-y-14 md:grid-cols-12 md:gap-x-6">
+					<!-- ASIDE -->
+					<div class="cars-page__aside-col col-span-4 md:col-span-4">
+						<AsideBar />
+					</div>
+					<!-- CARDS -->
+					<div class="cars-page__feed-col col-span-4 md:col-span-8">
+						<NuxtPage />
+					</div>
+				</div>
+			</UContainer>
+		</main>
+	</div>
+</template>

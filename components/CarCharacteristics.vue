966c45732b0c05103951a9e3146e3b1c220f4ecd
alex-bulanov.mail <alex-bulanov.mail@yandex.ru>
2024-06-21 10:38:46 +0300

    Страница машины. Компонент характеристик

--- components/CarCharacteristics.vue
+++ components/CarCharacteristics.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts"></script>
 
 <template>
-	<div class="car__characteristics">
-		<div class="car__characteristics-wrapper flex flex-col space-y-4">
-			<div class="car__characteristic border-b pb-4">
-				<div class="car__characteristic-wrapper flex items-center space-x-4">
-					<p class="car__characteristic-icon">✓</p>
-					<p class="car__characteristic-text text-sm">Leather interior</p>
-				</div>
+	<div class="car-characteristics">
+		<div class="car-characteristics__wrapper border-b pb-4 flex flex-col space-y-1">
+			<div class="car-characteristics__row flex items-center space-x-4">
+				<p class="car-characteristics__row-icon">✓</p>
+				<p class="car-characteristics__row-value text-sm">Leather interior</p>
+			</div>
+			<div class="car-characteristics__row flex items-center space-x-4">
+				<p class="car-characteristics__row-icon">✓</p>
+				<p class="car-characteristics__row-value text-sm">Leather interior</p>
 			</div>
 		</div>
 	</div>

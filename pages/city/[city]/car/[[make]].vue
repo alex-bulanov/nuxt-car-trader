d0d7718b1f58286299830fe770a3d357b41cad26
alex-bulanov.mail <alex-bulanov.mail@yandex.ru>
2024-06-21 09:46:02 +0300

    Динамические и необязательные пути

---
+++ pages/city/[city]/car/[[make]].vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+	<div class="page-make">
+		<main class="page-make__main py-[100px]">
+			<UContainer>
+				<p>Make</p>
+			</UContainer>
+		</main>
+	</div>
+</template>

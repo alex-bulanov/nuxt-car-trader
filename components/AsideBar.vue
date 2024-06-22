7a43b6206181a243cbb6109528b061b49b70c0ab
alex-bulanov.mail <alex-bulanov.mail@yandex.ru>
2024-06-22 20:29:48 +0300

    Страница каталога. Обновление местоположения

--- components/AsideBar.vue
+++ components/AsideBar.vue
@@ -1,19 +1,43 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const route = useRoute()
+
+const modal = reactive({
+	make: false,
+	location: false,
+	price: false
+})
+
+const city = ref<string>()
+
+const handleUpdateModal = (key: keyof typeof modal) => {
+	modal[key] = !modal[key]
+}
+
+const handleChangeLocation = () => {
+	if (!city.value) return
+	handleUpdateModal('location')
+	navigateTo(`/city/${city.value}/car/${route.params.make}`)
+	city.value = ''
+}
+</script>
 
 <template>
 	<div class="aside-bar border min-h-[200px] shadow">
 		<div class="aside-bar__wrapper flex flex-col min-h-[200px]">
 			<div class="aside-bar__row relative grow border-b flex justify-between space-x-4 p-5">
 				<div class="aside-bar__row-title font-semibold">Location</div>
-				<div class="aside-bar__row-value text-blue-400 capitalize">Toronto</div>
-				<!-- <div
-										class="absolute top-full right-0 border w-full p-5 shadow md:top-1/2 md:-right-[calc(100%_+_16px)]"
-									>
-										<UInput />
-										<div class="mt-4">
-											<UButton block label="Apply" />
-										</div>
-									</div> -->
+				<div class="aside-bar__row-value text-blue-400 capitalize" @click="handleUpdateModal('location')">
+					{{ route.params.city }}
+				</div>
+				<div
+					v-if="modal.location"
+					class="absolute top-full right-0 border w-full p-5 bg-white shadow md:top-1/2 md:-right-[calc(100%_+_16px)]"
+				>
+					<UInput v-model="city" />
+					<div class="mt-4">
+						<UButton block label="Apply" @click="handleChangeLocation" />
+					</div>
+				</div>
 			</div>
 			<div class="aside-bar__row relative grow border-b flex justify-between space-x-4 p-5">
 				<div class="aside-bar__row-title font-semibold">Make</div>

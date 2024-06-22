2d1030f0487eb56789b6a9843d10d8e285041527
alex-bulanov.mail <alex-bulanov.mail@yandex.ru>
2024-06-22 18:02:41 +0300

    Страница ошибки

---
+++ error.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+// eslint-disable-next-line vue/require-default-prop
+defineProps({ error: Object })
+
+const handleError = () => {
+	clearError({
+		redirect: '/'
+	})
+}
+</script>
+
+<template>
+	<div class="page-error">
+		<main class="page-error__main">
+			<div class="page-error__wrapper flex flex-col justify-center h-[100svh]">
+				<UContainer>
+					<h1 class="page-error__title mb-4 text-3xl text-center">Error: {{ error?.statusCode }}</h1>
+					<p class="page-error__message mb-2 text-center">{{ error?.statusMessage }}</p>
+
+					<div class="page-error__actions text-center">
+						<UButton size="xl" label="На Главную" @click="handleError" />
+					</div>
+				</UContainer>
+			</div>
+		</main>
+	</div>
+</template>
